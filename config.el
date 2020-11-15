91337404a36c0a6b4d2ec9116270775cd8c79cf5
jakeisnt <jakechv@github.com>
2020-11-15 20:40:31 +0100

    Ranger not working

--- config.el
+++ config.el
@@ -179,17 +179,17 @@
 
 ")))
 
-(use-package! ranger
-  :init
-  (ranger-override-dired-mode t)
-  :config
-  (setq
-   ranger-cleanup-on-disable t
-   ranger-modify-header t
-   ranger-hide-cursor t
-   ranger-preview-file t
-   ranger-max-preview-size 20
-   ranger-dont-show-binary t))
+;; (use-package! ranger
+;;   ;; :init
+;;   ;; (ranger-override-dired-mode t)
+;;   :config
+;;   (setq
+;;    ranger-cleanup-on-disable t
+;;    ranger-modify-header t
+;;    ranger-hide-cursor t
+;;    ranger-preview-file t
+;;    ranger-max-preview-size 20
+;;    ranger-dont-show-binary t))
 
 
 (defun ido-kill-emacs-hook ()
