b7d3cf75870adf86860bad3cf8661ec350aa456d
Jacob Chvatal <jake@isnt.online>
2022-09-20 22:04:39 -0400

    slightly better theme

--- config.el
+++ config.el
@@ -20,7 +20,7 @@
 ;;; Code:
 (setq user-full-name "Jacob Chvatal"
       user-mail-address "jakechvatal@gmail.com"
-      doom-theme 'doom-plain-dark)
+      doom-theme 'doom-badger)
 
 (setq-default delete-by-moving-to-trash t
               tab-width 4
