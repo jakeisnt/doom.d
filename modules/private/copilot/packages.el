e8cf6c7fd54085af4a7d35255aa3c67fc87b40cd
Jacob Chvatal <jake@isnt.online>
2023-02-15 16:53:42 -0500

    disable emmet-mode

--- modules/private/copilot/packages.el
+++ modules/private/copilot/packages.el
@@ -5,4 +5,9 @@
   :recipe (:host github :repo "zerolfx/copilot.el"
                    :files ("dist" "copilot.el")))
 
+;; The `tab` keybinding conflicts here, so disable it.
+;; If we want `emmet-mode` in the future, use the following line instead:
+;; (map! :after emmet-mode [tab] nil)
+(package! emmet-mode :disable t)
+
 (provide 'packages)
