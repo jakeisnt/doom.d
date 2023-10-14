cf64733df8e6b84a82481e3355507a5af86d2b31
Jacob Chvatal <jake@isnt.online>
2023-10-14 17:14:31 +0200

    add tree-sitter support for js

--- init.el
+++ init.el
@@ -119,7 +119,7 @@
  ;; idris        ; idris2 is better!!!!!!!!!!!!!!
  json         ; At least it ain't XML
  ;; (java +meghanada) ; the poster child for carpal tunnel syndrome
- (javascript +lsp)
+ (javascript +lsp +tree-sitter)
  ;;julia             ; a better, faster MATLAB
  ;; kotlin            ; a better, slicker Java(Script)
  latex                           ; writing papers in Emacs has never been so fun
